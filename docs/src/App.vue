28e950b7e2498a7fc1822971f2a539327db71e83
Thomas Hansen <thomasofdenmark@gmail.com>
2023-02-03 16:25:57 +0100

    Navigation tweek

--- docs/src/App.vue
+++ docs/src/App.vue
@@ -1,101 +1,100 @@
-<script setup lang="ts">
-
-// import 'bootstrap/js/dist/offcanvas.js'
-// import 'bootstrap/js/dist/dropdown.js'
-
-</script>
-
 <template>
-  <div class="d-flex flex-column min-vh-100;">
-    <header class="d-flex align-items-center p-3 px-5 fl-bg-white">
-      <router-link class="d-block" to="/">
-        <img
-          class="w-auto"
-          style="height: 30px"
-          src="/src/assets/frank-and-live-styleguide/dist/static/logos/logo.svg"
-          alt="FRANK & LIVE"
-        />
-      </router-link>
-      <p class="m-0 ms-3">
-        Styleguide
-      </p>
-    </header>
-    <div class="row flex-grow-1 p-5">
-      <aside class="col-3">
-        <nav>
-          <ul class="docs-menu">
-            <li class="mb-2">
+  <nav class="navbar navbar-expand-md p-3 px-5 fl-bg-white" style="z-index: 124;">
+    <div class="container-fluid">
+      <div class="navbar-brand">
+        <router-link class="d-block" to="/">
+          <img
+            class="w-auto"
+            style="height: 30px"
+            src="/src/assets/frank-and-live-styleguide/dist/static/logos/logo_fnl.svg"
+            alt="FRANK & LIVE"
+          />
+        </router-link>
+      </div>
+      <span class="flex-grow-1"></span>
+      <button class="fl-btn fl-btn-link fl-fs-p d-md-none me-4 collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#sidebarMenu" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="show-collapsed">
+          Menu
+        </span>
+        <span class="hide-collapsed">
+          Close
+        </span>
+      </button>
+    </div>
+  </nav>
+  <div class="container-fluid">
+    <div class="row">
+      <nav class="col-md-4 col-lg-3 col-xxl-2 collapse d-md-block fl-bg-white" id="sidebarMenu">
+        <div class="pt-3 sidebar-sticky">
+          <ul class="p-5 pt-3 fl-fs-p sidebar-menu">
+            <li>
               <router-link to="/animations/">Animations</router-link>
             </li>
-            <li class="mb-2">
+            <li>
               <router-link to="/badges/">Badges</router-link>
             </li>
-            <li class="mb-2">
+            <li>
               <router-link to="/buttons/">Buttons</router-link>
             </li>
-            <li class="mb-2">
+            <li>
               <router-link to="/checks/">Checks & radios</router-link>
             </li>
-            <li class="mb-2">
+            <li>
               <router-link to="/colors/">Colors</router-link>
             </li>
-            <li class="mb-2">
+            <li>
               <router-link to="/inputs/">Inputs</router-link>
             </li>
-            <li class="mb-2">
+            <li>
               <router-link to="/logos/">Logos</router-link>
             </li>
-            <li class="mb-2">
+            <li>
               <router-link to="/selects/">Selects</router-link>
             </li>
-            <li class="mb-2">
+            <li>
               <router-link to="/shadows/">Shadows</router-link>
             </li>
-            <li class="mb-2">
+            <li>
               <router-link to="/spacing/">Spacing</router-link>
             </li>
-            <li class="mb-2">
+            <li>
               <router-link to="/tables/">Tables</router-link>
             </li>
-            <li class="mb-2">
+            <li>
               <router-link to="/text/">Text</router-link>
             </li>
-            <li class="mb-2">
+            <li>
               <router-link to="/textareas/">Textareas</router-link>
             </li>
           </ul>
-          <div class="flex-fill"></div>
-          <div class="dropdown text-end">
-            <a href="#" class="d-block link-dark text-decoration-none dropdown-toggle" data-bs-toggle="dropdown" aria-expanded="false">
-              <img src="https://github.com/mdo.png" alt="mdo" width="32" height="32" class="rounded-circle">
-            </a>
-            <ul class="dropdown-menu text-small" style="">
-              <li><a class="dropdown-item" href="#">New project...</a></li>
-              <li><a class="dropdown-item" href="#">Settings</a></li>
-              <li><a class="dropdown-item" href="#">Profile</a></li>
-              <li><hr class="dropdown-divider"></li>
-              <li><a class="dropdown-item" href="#">Sign out</a></li>
-            </ul>
-          </div>
-        </nav>
-      </aside>
-      <main class="col-9 col-xxl-8"><router-view /></main>
+        </div>
+      </nav>
+      <main class="col-md-8 col-lg-9 col-xxl-10 mt-5 ">
+        <router-view />
+      </main>
     </div>
   </div>
 </template>
 
 <style scoped>
-.docs-menu {
-  list-style: none;
-  margin: 0;
-  padding: 0;
-}
-.docs-menu a {
-  font-size: 24px;
-  text-decoration: none;
-}
-
-.docs-menu a.active {
-  color: var(--fl-color-green-500);
-}
+  .sidebar-sticky {
+    position: sticky;
+    top: 0;
+    height: calc(100vh - 65px);
+    overflow-x: hidden;
+    overflow-y: auto;
+  }
+  ul.sidebar-menu {
+    list-style: none;
+  }
+  ul.sidebar-menu li {
+    position: relative;
+    padding: 8px 0;
+  }
+  ul.sidebar-menu li a {
+    text-decoration: none;
+  }
+  ul.sidebar-menu li a.active {
+    color: var(--fl-color-green-500);
+  }
 </style>
